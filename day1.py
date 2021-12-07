5bca3c0500718ee60805d6029e0b0ccda59d5bf7
c3AlexVillarreal <alex.villarreal@c3.ai>
2021-12-07 20:20:27 +0000

    Custom pylint configuration

--- day1.py
+++ day1.py
@@ -1,13 +1,13 @@
-window_size = 3
-num_of_increases = 0
+WINDOW_SIZE = 3
+num_of_increases = 0 # pylint: disable=invalid-name
 
 measurements = []
 with open('day1.input') as f:
     measurements = [int(line) for line in f]
 
-for i in range(0,len(measurements) - window_size):
-    prev_window_sum = sum(measurements[i:i+window_size])
-    current_window_sum = sum(measurements[i+1:i+1+window_size])
+for i in range(0,len(measurements) - WINDOW_SIZE):
+    prev_window_sum = sum(measurements[i:i+WINDOW_SIZE])
+    current_window_sum = sum(measurements[i+1:i+1+WINDOW_SIZE])
     if current_window_sum > prev_window_sum:
         num_of_increases += 1
 
--- day2.py
+++ day2.py
@@ -4,9 +4,12 @@ with open('day2.input') as f:
         pieces = line.split()
         instructions.append( (pieces[0], int(pieces[1])) )
 
+# pylint: disable=invalid-name
 aim = 0
 horizontal_coord = 0
 vertical_coord = 0
+# pylint: enable=invalid-name
+
 for i in instructions:
     if i[0] == 'forward':
         horizontal_coord += i[1]
