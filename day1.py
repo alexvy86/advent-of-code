73e63f3e8da7cb7e6633e2330f107962cbb0635a
c3AlexVillarreal <alex.villarreal@c3.ai>
2021-12-07 20:02:54 +0000

    Day 1 - part 2

--- day1.py
+++ day1.py
@@ -1,9 +1,14 @@
+window_size = 3
 num_of_increases = 0
-last_value = None
+
+measurements = []
 with open('day1.input') as f:
-    for depth in (int(line) for line in f):
-        if last_value is not None and depth > last_value:
-            num_of_increases += 1
-        last_value = depth
+    measurements = [int(line) for line in f]
+
+for i in range(0,len(measurements) - window_size):
+    prev_window_sum = sum(measurements[i:i+window_size])
+    current_window_sum = sum(measurements[i+1:i+1+window_size])
+    if current_window_sum > prev_window_sum:
+        num_of_increases += 1
 
 print(num_of_increases)
