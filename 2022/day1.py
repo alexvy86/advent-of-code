8c8a0183efbe7e165cf7305d6ab45b5a4c06bc37
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-02 06:43:04 +0000

    AOC 2022, Day 1,  Part 1

---
+++ 2022/day1.py
@@ -0,0 +1,11 @@
+items = []
+current_elfs_items = []
+with open('day1.input') as f:
+    for line in f.read().splitlines():
+        if line == "":
+            items.append(current_elfs_items)
+            current_elfs_items = []
+        else:
+            current_elfs_items.append(int(line))
+
+print(max(sum(elfs_items) for elfs_items in items))
