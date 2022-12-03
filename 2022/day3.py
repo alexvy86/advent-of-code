5b21202ef286ce2837baab6eb77651f21c501a2f
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-03 05:27:04 +0000

    AOC 2022, Day 3, Part 2

--- 2022/day3.py
+++ 2022/day3.py
@@ -1,4 +1,5 @@
 total_priority = 0
+total_badge_priority = 0
 
 with open('day3.input') as f:
     for line in f.read().splitlines():
@@ -16,3 +17,29 @@ with open('day3.input') as f:
                 break
 
 print(total_priority)
+
+with open('day3.input') as f:
+    group = []
+    for line in f.read().splitlines():
+        group.append(line)
+
+        if len(group) < 3:
+            continue
+
+        elf1 = set(group[0])
+        elf2 = set(group[1])
+        elf3 = set(group[2])
+
+        for letter in elf1:
+            if letter in elf2 and letter in elf3:
+                if letter < 'a':
+                    priority = ord(letter) - 38
+                else:
+                    priority = ord(letter) - 96
+                # print(f"{letter} {priority}")
+                total_badge_priority += priority
+                break
+
+        group.clear()
+
+print(total_badge_priority)
