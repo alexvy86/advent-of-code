fb24d8c2fce9d148b093dcd23d6a377e998a35c9
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-17 17:42:42 +0000

    AoC 2022 - Day 7 - Part 2

--- 2022/day7.py
+++ 2022/day7.py
@@ -35,14 +35,24 @@ with open('day7.input') as f:
             else:
                 current_node.children[name] = Node(name, int(size_or_dir))
 
+total_space = 70000000
+needed_free_space = 30000000
+current_free_space = total_space - root.totalSize()
+min_size_of_dir_to_delete = root.totalSize()
+
 dirs_to_check = [root]
 sum_of_sizes = 0
 while len(dirs_to_check) > 0:
     current_dir = dirs_to_check.pop()
     for (_, c) in current_dir.children.items():
         if c.size == 0: # It's a dir
-            if c.totalSize() < 100000:
-                sum_of_sizes += c.totalSize()
+            dir_size = c.totalSize()
+            if dir_size < 100000:
+                sum_of_sizes += dir_size
             dirs_to_check.append(c)
+            if current_free_space + dir_size > needed_free_space and \
+                dir_size < min_size_of_dir_to_delete:
+                min_size_of_dir_to_delete = dir_size
 
 print(sum_of_sizes)
+print(min_size_of_dir_to_delete)
