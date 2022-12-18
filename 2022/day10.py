bef8393c719d95c39df526a5f090a3c48753f80e
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-18 07:24:31 +0000

    AoC 2022 - Day 10 - Part 2

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,24 +1,32 @@
 register = 1
-current_cycle = 1
+current_cycle = 0
 total_signal_strength = 0
+
+screen = [['.']*40,['.']*40,['.']*40,['.']*40,['.']*40,['.']*40]
+
+def print_pixel():
+    r = current_cycle // 40
+    col = current_cycle % 40
+    char = '#' if (current_cycle % 40) + 1 in range(register, register + 3) else '.'
+    screen[r][col] = char
+
 with open('day10.input') as f:
     for line in f.read().splitlines():
-        arg = 0
-        if line == "noop":
-            op = line
-        else:
-            (op, arg) = line.split(" ")
+        (op, arg) = (line, 0) if line == "noop" else line.split(" ")
 
-        if current_cycle in [20,60,100,140,180,220]:
-            total_signal_strength += current_cycle * register
+        if (current_cycle + 1) in [20,60,100,140,180,220]:
+            total_signal_strength += (current_cycle + 1) * register
+
+        print_pixel()
 
         if op == "noop":
             current_cycle += 1
         elif op == "addx":
             current_cycle += 1
 
-            if current_cycle in [20,60,100,140,180,220]:
-                total_signal_strength += current_cycle * register
+            if (current_cycle + 1) in [20,60,100,140,180,220]:
+                total_signal_strength += (current_cycle + 1) * register
+            print_pixel()
 
             current_cycle += 1
             register += int(arg)
@@ -26,3 +34,5 @@ with open('day10.input') as f:
             raise ValueError(f"Unsupported operation {op}")
 
 print(total_signal_strength)
+for row in screen:
+    print("".join(row))
